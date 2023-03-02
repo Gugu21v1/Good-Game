5d31c01f96d0c8846ab56b92bb5ec75107176751
LuizGustavo <lgfs2903@gmail.com>
2023-03-02 13:51:30 -0300

    Navbar, Added Pundit, Fixed Controllers, Did the forms, Put some instructions in the views, database working.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @user = User.all.last
+    @user = current_user
+    @users = User.all
   end
 end
