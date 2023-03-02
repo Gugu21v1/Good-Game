630529ff84c12d74f958a8ecec26a90b720a0ad9
LuizGustavo <lgfs2903@gmail.com>
2023-03-02 11:21:11 -0300

    User working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get 'user/:id', to: 'users#show', as: 'user'
   devise_for :users do
     resources :products
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_185137) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_140205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_185137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "product_url"
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_products_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_185137) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "inters", "products"
   add_foreign_key "inters", "users"
+  add_foreign_key "products", "users", column: "users_id"
 end
