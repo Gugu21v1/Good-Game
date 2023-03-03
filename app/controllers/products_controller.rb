81e4607ebc6512391be027eca35abc05bf8dd27c
LuizGustavo <lgfs2903@gmail.com>
2023-03-03 17:39:58 -0300

    ajustes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @user = User.find(@product.user_id)
+    @user = User.find(@product.users_id)
     authorize @product
    end
 
